a0efb71f38d396d56110c382386da04bb57cf7d0
Davis Kane <daviskane@MacBook-Pro.local>
2020-10-09 18:53:55 -0400

    Updates

---
+++ precache-manifest.0e47640dbafd905a7d606134248681a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d8b33aac541ed9ddb951b1adde2207",
+    "url": "/kittyfriends/index.html"
+  },
+  {
+    "revision": "829d509845f74bb74b91",
+    "url": "/kittyfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "73832af91324b71359f1",
+    "url": "/kittyfriends/static/css/main.1cd073f0.chunk.css"
+  },
+  {
+    "revision": "829d509845f74bb74b91",
+    "url": "/kittyfriends/static/js/2.73608afc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittyfriends/static/js/2.73608afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73832af91324b71359f1",
+    "url": "/kittyfriends/static/js/main.9e90399a.chunk.js"
+  },
+  {
+    "revision": "5bde646a67b14e7faceb",
+    "url": "/kittyfriends/static/js/runtime-main.9eac5891.js"
+  }
+]);
