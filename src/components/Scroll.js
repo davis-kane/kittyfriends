f24220788e4305cb82d95ca12897a7a721876193
Davis Kane <daviskane@MacBook-Pro.local>
2020-10-09 19:14:23 -0400

    Fixed hidden content on scroll w/ padding

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
   return (
-    <div style={{ overflowY: 'scroll', height: '800px'}}>
+    <div style={{ overflowY: 'scroll', height: '800px', paddingBottom: '150px'}}>
       {props.children}
     </div>
   )
